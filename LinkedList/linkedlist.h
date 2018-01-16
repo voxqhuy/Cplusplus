19a1e10b9c3bb854d82424222b739698e07dfa44
voxqhuy <kohnewlife@gmail.com>
2018-01-16 14:40:32 -0500

    added indexing operator and prepend method

--- LinkedList/linkedlist.h
+++ LinkedList/linkedlist.h
@@ -1,160 +1,211 @@
- // linkedlist.h
+// linkedlist.h
 
- #include <iostream>
- #include <utility> // For std::swap
- #include <memory>  // For std::shared_ptr
- 
- template <typename T>
- class LinkedList {
-     // The nested private Node class from before
-     struct Node {
-         T data;                      // A data element of the list
-         std::shared_ptr<Node> next;  // The node that follows this one
-         // Constructor
-         Node(const T& item): data(item), next(nullptr) {}
-     };
- 
-     std::shared_ptr<Node> head;  // Points to the first item in the list
-     std::shared_ptr<Node> tail;  // Points to the last item in the list
+#include <iostream>
+#include <utility> // For std::swap
+#include <memory>  // For std::shared_ptr
 
-     int len;                     // Number of elements in the list
- 
- public:
-     // The constructor makes an initially empty list.
-     // The list is empty when head and tail are null.
-     LinkedList(): head(nullptr), tail(nullptr), len(0) {}
+template <typename T>
+class LinkedList
+{
+    // The nested private Node class from before
+    struct Node
+    {
+        T data;                     // A data element of the list
+        std::shared_ptr<Node> next; // The node that follows this one
+        // Constructor
+        Node(const T &item) : data(item), next(nullptr) {}
+    };
 
-     // Copy constructor makes a copy of the other object's list
-     LinkedList(const LinkedList& other): LinkedList() {
-         // Walk through other's list inserting each of its elements
-         // into this list
-         for (auto cursor = other.head; cursor; cursor = cursor->next)
-             insert(cursor->data);
-     }
+    std::shared_ptr<Node> head; // Points to the first item in the list
+    std::shared_ptr<Node> tail; // Points to the last item in the list
 
-     // Move constructor takes possession of the temporary's list
-     LinkedList(LinkedList&& temp): LinkedList() {
-         // Swap contents with the temporary
-         std::swap(head, temp.head);
-         std::swap(tail, temp.tail);
-         std::swap(len, temp.len);
-     }
+    int len; // Number of elements in the list
 
-     // Assignment operator
-     LinkedList& operator=(const LinkedList& other) {
-         // Make a local, temporary copy of other
-         LinkedList temp{other};
-         // Exchange the head and tail pointers and len from this list
-         // with those of the new, temporary list
-         std::swap(head, temp.head);
-         std::swap(tail, temp.tail);
-         std::swap(len, temp.len);
-         // The temporary list now points to this list's original contents, 
-         // and this list now points to the copy of other's list
-         // The temporary list will be destroyed since it is a temporary 
-         return *this;
-     }
+  public:
+    // The constructor makes an initially empty list.
+    // The list is empty when head and tail are null.
+    LinkedList() : head(nullptr), tail(nullptr), len(0) {}
 
-     // Move assignment operator
-     LinkedList& operator=(LinkedList&& temp) {
-         // Exchange the head and tail pointers and len from this list
-         // with those of the new, temporary list
-         std::swap(head, temp.head);
-         std::swap(tail, temp.tail);
-         std::swap(len, temp.len);
-         // The temporary list now points to this list's original contents, 
-         // and this list now points to the temporary's list
-         // The temporary list will be destroyed since it is a temporary
-         return *this;
-     }
- 
-     // The destructor deallocates the memory held by the list
-     ~LinkedList() { 
-         clear(); 
-     }
- 
-     // Inserts item onto the back of the list.
-     // item is the element to insert.
-     void insert(const T& item) {
-         // Make a node for the new element n
-         auto new_node = std::make_shared<Node>(item);
-         if (tail) {   // Is tail non-null?
-            tail->next = new_node;  // Link the new node onto the back
-            tail = new_node;        // The new node is the new tail of the list
-         }
-         else // List is empty, so make head and tail point to new node
-             head = tail = new_node;
-         len++;
-     }
+    // Copy constructor makes a copy of the other object's list
+    LinkedList(const LinkedList &other) : LinkedList()
+    {
+        // Walk through other's list inserting each of its elements
+        // into this list
+        for (auto cursor = other.head; cursor; cursor = cursor->next)
+            insert(cursor->data);
+    }
 
-     // Removes the first occurrence of item from the list.
-     // Returns true if successful (found item and removed it).
-     // Returns false if item is not originally present in the list.
-     bool remove(const T& item) {
-         auto cursor = head,    // Start at head of list
-              prev =  head;     // Keep track of previous node seen
-         // Loop until we run off the end of the list or find n,
-         // whichever comes first
-         while (cursor && cursor->data != item) {
-             prev = cursor;              // Remember previous node
-             cursor = cursor->next;      // Move to next node
-         }
-         if (!cursor)                    // Did we run off the end of the list?
-             return false;               // Indicate we did not find n
+    // Move constructor takes possession of the temporary's list
+    LinkedList(LinkedList &&temp) : LinkedList()
+    {
+        // Swap contents with the temporary
+        std::swap(head, temp.head);
+        std::swap(tail, temp.tail);
+        std::swap(len, temp.len);
+    }
 
-         // Found n; cursor is pointing at the node containing n 
-         if (head == tail)               // n was the only element in the list
-             head = tail = nullptr;      // cursor still points to node with n
-         else if (cursor == head)        // Is n the first element in the list?
-             head = head->next;          // Redirect head around n
-         else                            // n is not the first element
-             prev->next = cursor->next;  // Redirect previous node around n
-         if (cursor == tail)             // Was n the last element in the list?
-             tail = prev;                // Update tail to new last element
+    // The indexing operator allows a client to access an element
+    // by its offset from the beginning of the list
+    std::shared_ptr<Node> operator[](int index)
+    {
+        auto cursor = head, // Start at head of list
+            prev = head;    // Keep track of previous node seen
+        // Loop until the cursor goes to the element at position "index" in the list
+        // start from the beginning of the list
+        for (int i = 0; i < index; ++i)
+        {
+            prev = cursor;         // Remember previous node
+            cursor = cursor->next; // Move to next node
+        }
+        // Return the node for the element the client wants to access
+        return cursor;
+    }
 
-         // No need to delete node; shared_ptr takes care of it
-         len--;                          // List size decreases by 1
-         return true;                    // We found n and deleted its node
-     }
+    // Assignment operator
+    LinkedList &operator=(const LinkedList &other)
+    {
+        // Make a local, temporary copy of other
+        LinkedList temp{other};
+        // Exchange the head and tail pointers and len from this list
+        // with those of the new, temporary list
+        std::swap(head, temp.head);
+        std::swap(tail, temp.tail);
+        std::swap(len, temp.len);
+        // The temporary list now points to this list's original contents,
+        // and this list now points to the copy of other's list
+        // The temporary list will be destroyed since it is a temporary
+        return *this;
+    }
 
-     // Prints the contents of the linked list of integers.
-     void print() const {
-         for (auto cursor = head; cursor; cursor = cursor->next) 
-             std::cout << cursor->data << ' '; 
-         std::cout << '\n';
-     }
- 
-     // Returns the length of the linked list.
-     int length() const {
-         return len;
-     }
- 
-     // Removes all the elements in the linked list.
-     void clear() {
-         head = tail = nullptr;  // Null head signifies list is empty
-         len = 0;
-     }
+    // Move assignment operator
+    LinkedList &operator=(LinkedList &&temp)
+    {
+        // Exchange the head and tail pointers and len from this list
+        // with those of the new, temporary list
+        std::swap(head, temp.head);
+        std::swap(tail, temp.tail);
+        std::swap(len, temp.len);
+        // The temporary list now points to this list's original contents,
+        // and this list now points to the temporary's list
+        // The temporary list will be destroyed since it is a temporary
+        return *this;
+    }
 
-     // Provide a convenient way to print a linked list
-     template <typename V>
-     friend std::ostream& operator<<(std::ostream& os, 
-                                     const LinkedList<V>& list);
- };
+    // The destructor deallocates the memory held by the list
+    ~LinkedList()
+    {
+        clear();
+    }
 
- // Prints a linked list object to an output stream
- template <typename T>
- std::ostream& operator<<(std::ostream& os, const LinkedList<T>& list) {
-     os << '{';
-     auto cursor = list.head;
-     if (cursor) {
-         os << cursor->data;
-         cursor = cursor->next;
-         while (cursor) {
-             os << ", " << cursor->data;
-             cursor = cursor->next;
-         }
-     }
-     os << '}';
-     return os;
- }
+    // Inserts item onto the back of the list.
+    // item is the element to insert.
+    void insert(const T &item)
+    {
+        // Make a node for the new element n
+        auto new_node = std::make_shared<Node>(item);
+        if (tail)
+        {                          // Is tail non-null?
+            tail->next = new_node; // Link the new node onto the back
+            tail = new_node;       // The new node is the new tail of the list
+        }
+        else // List is empty, so make head and tail point to new node
+            head = tail = new_node;
+        len++;
+    }
 
+    // Appends an element onto the front of the list.
+    // item is the element to append.
+    void prepend(const T &item)
+    {
+        // Make a node for the new element n
+        auto new_node = std::make_shared<Node>(item);
+        if (head)
+        { // Is tail non-null?
+            head->next = head;
+            head = new_node;
+        }
+        else // List is empty, so make head and tail point to new node
+            head = tail = new_node;
+        len++;
+    }
+
+
+
+    // Removes the first occurrence of item from the list.
+    // Returns true if successful (found item and removed it).
+    // Returns false if item is not originally present in the list.
+    bool remove(const T &item)
+    {
+        auto cursor = head, // Start at head of list
+            prev = head;    // Keep track of previous node seen
+        // Loop until we run off the end of the list or find n,
+        // whichever comes first
+        while (cursor && cursor->data != item)
+        {
+            prev = cursor;         // Remember previous node
+            cursor = cursor->next; // Move to next node
+        }
+        if (!cursor)      // Did we run off the end of the list?
+            return false; // Indicate we did not find n
+
+        // Found n; cursor is pointing at the node containing n
+        if (head == tail)              // n was the only element in the list
+            head = tail = nullptr;     // cursor still points to node with n
+        else if (cursor == head)       // Is n the first element in the list?
+            head = head->next;         // Redirect head around n
+        else                           // n is not the first element
+            prev->next = cursor->next; // Redirect previous node around n
+        if (cursor == tail)            // Was n the last element in the list?
+            tail = prev;               // Update tail to new last element
+
+        // No need to delete node; shared_ptr takes care of it
+        len--;       // List size decreases by 1
+        return true; // We found n and deleted its node
+    }
+
+    // Prints the contents of the linked list of integers.
+    void print() const
+    {
+        for (auto cursor = head; cursor; cursor = cursor->next)
+            std::cout << cursor->data << ' ';
+        std::cout << '\n';
+    }
+
+    // Returns the length of the linked list.
+    int length() const
+    {
+        return len;
+    }
+
+    // Removes all the elements in the linked list.
+    void clear()
+    {
+        head = tail = nullptr; // Null head signifies list is empty
+        len = 0;
+    }
+
+    // Provide a convenient way to print a linked list
+    template <typename V>
+    friend std::ostream &operator<<(std::ostream &os,
+                                    const LinkedList<V> &list);
+};
+
+// Prints a linked list object to an output stream
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const LinkedList<T> &list)
+{
+    os << '{';
+    auto cursor = list.head;
+    if (cursor)
+    {
+        os << cursor->data;
+        cursor = cursor->next;
+        while (cursor)
+        {
+            os << ", " << cursor->data;
+            cursor = cursor->next;
+        }
+    }
+    os << '}';
+    return os;
+}
