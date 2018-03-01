5971cf4ced42d8328b3a0f76a5198d5ce6968594
voxqhuy <kohnewlife@gmail.com>
2018-03-01 00:54:16 -0500

    make the printFrequency function

--- Huffman Tree/main.cpp
+++ Huffman Tree/main.cpp
@@ -2,17 +2,35 @@
 #include <iostream>
 using namespace std;
 
+void printFrequency(int* freq) {
+
+}
+
 int main() {
-    char x;
+    // each letter
+    char letter;
+    // An array to keep the frequencies
+    int freq[90] = { }; // initialize all frequencies to 0
+
+    // Using the text file as input
     ifstream inFile;
     inFile.open("D:\\iRoommm\\Web dev\\Github copies\\C-\\Huffman Tree\\declaration.text");
-
+    // Check if the text file could be opened
     if (!inFile) {
-        cerr  << "Unable to open file declaration.text";
+        std::cout  << "Unable to open file declaration.text";
         exit(1);        // Unable to open the file, exit the function
     }
-    while (inFile >> x) {
-        putchar (std::toupper(x));
+    // Reading the text
+    inFile >> noskipws;     // including the whitespace
+    while (inFile >> letter) {
+        std::cout << letter;        // printing out the texts
+        freq[std::toupper(letter)]++;       // adding 1 to the frequency of the letter (capitialized)
     }
+
+    std::cout << "\n" << "Counts:" << "\n" << "-------" << "\n";
+    // print out the frequencies
+    printFrequency(freq);
+
     inFile.close();
 }
+
