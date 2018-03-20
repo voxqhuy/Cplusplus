7005f61b6106eb0335be011a9a663579f336720a
voxqhuy <kohnewlife@gmail.com>
2018-03-19 23:31:34 -0400

    update HashTable class

--- Hard/Hash Table/main.cpp
+++ Hard/Hash Table/main.cpp
@@ -6,6 +6,7 @@
 //  Honor statement: I have neither given nor received any unauthorized help on this assignment. 
 
 #include <iostream>     // std::cout, std::fixed
+#include <vector>       // std::vector
 #include <string>
 using namespace std;
 
@@ -16,8 +17,39 @@ private:
 public:
     // Constructor
     HashTable (int tableSize, string fileName): mTableSize(tableSize), mFileName(fileName) {}
+
+    // hash function maps a string to an index within the table
+    void hash() {
+
+    }
+
+    // insert method inserts a string into the hash table
+    // return true if it successfully inserts the word into the table
+    // otherwise, it should return false if the word already is present in the table
+    bool insert() {
+        return false;
+    }
+
+    // remove method removes a string from the hash table 
+    // returns true if it removes the word
+    // otherwise, it returns false if the word to remove is not in the hash table
+    bool remove() {
+        return false;
+    }
+
+    // returns true if the hash table contains a given string
+    // otherwise, the method returns false if the string is not present
+    bool contains() {
+
+    }
+
+    // returns the total number of strings in the hash table
+    int size() {
+        return mTableSize;
+    }
 }
 
 int main() {
+    HashTable hashTable = new HashTable(109582, )
     return 0;
 }
