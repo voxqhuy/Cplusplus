a3c80d092b995f988a92696dd96459ae0146e6ff
voxqhuy <kohnewlife@gmail.com>
2018-03-23 17:25:34 -0400

    changed some prompts for more user-friendly, changed some variables' names

--- Hard/Hash Table/hashing-with-vector.cpp
+++ Hard/Hash Table/hashing-with-vector.cpp
@@ -11,7 +11,6 @@
 #include <string>       // std::string
 #include <queue>        // std::priority_queue
 #include <cstddef>      // std::size_t
-#include <algorithm>
 using namespace std;
 
 // HashTable class
@@ -24,10 +23,7 @@ public:
     /***    CONSTRUCTOR    ***/
     HashTable (unsigned tableSize, string fileName) : nodesTable(tableSize) {
         this->tableSize = tableSize;
-        numElement = 0;         // Intial size = 0
-
-        // Initialize the size of hash table
-        // nodesTable.reserve(tableSize);
+        numElement = 0;         // Intial number of elements = 0
 
         string word;            // each word in the text file
         ifstream inFile;        
@@ -72,7 +68,6 @@ public:
     bool insert(string word) {
         // the index of the word in the hash table
         unsigned index = hash(word); 
-        cout << index << ' ';
         // the chain at that index
         vector<string>& chain = nodesTable[index];
         // check if the word is already present
@@ -82,6 +77,7 @@ public:
                     return false;   // the word is already present
             }
         }
+        cout << word << "\n";
 
         chain.push_back(word);      // add a new node
         // successfully inserted the word
@@ -112,7 +108,7 @@ public:
 
     // returns true if the hash table contains a given word
     // otherwise, the method returns false if the word is not present
-    bool contains(string word) const { 
+    bool contain(string word) const { 
         // the index of the word in the hash table
         unsigned index = hash(word); 
         // the chain at that index
@@ -145,14 +141,14 @@ public:
 
 // checking for an omission
 void checkOmission(string userInput, const HashTable& hashTable, 
-    priority_queue<string, std::vector<string>, CompareAlphabets>& correctedWords) {  
+    priority_queue<string, std::vector<string>, CompareAlphabets>& suggestedWords) {  
     // Try adding a single letter at all possible places in the word
     // If any match a word in the hash table, push into the queue for printing later
     for (size_t i = 0; i <= userInput.length(); i++) {
         for (char j = 'a'; j <= 'z'; j++) {
             string testingWord = userInput;     // make a copy of userInput to test 
-            if (hashTable.contains(testingWord.insert(i, string(1, j)))) { // FIXME: j doesn't work, has to convert j to string??
-                correctedWords.push(testingWord);
+            if (hashTable.contain(testingWord.insert(i, string(1, j)))) { // FIXME: j doesn't work, has to convert j to string??
+                suggestedWords.push(testingWord);
             }
         }
     }
@@ -160,27 +156,27 @@ void checkOmission(string userInput, const HashTable& hashTable,
 
 // checking for an extra letter
 void checkExtra(string userInput, const HashTable& hashTable, 
-    priority_queue<string, std::vector<string>, CompareAlphabets>& correctedWords) {
+    priority_queue<string, std::vector<string>, CompareAlphabets>& suggestedWords) {
     // Try removing a single letter from the word
     // If any match a word in the hash table, push into the queue for printing later
     for (size_t i = 0; i < userInput.length(); i++) {
         string testingWord = userInput;         // make a copy of userInput to test
-        if (hashTable.contains(testingWord.erase(i, 1))) {
-            correctedWords.push(testingWord);
+        if (hashTable.contain(testingWord.erase(i, 1))) {
+            suggestedWords.push(testingWord);
         }
     }
 }
 
 // checking for a typo
 void checkTypo(string userInput, const HashTable& hashTable, 
-    priority_queue<string, std::vector<string>, CompareAlphabets>& correctedWords) {
+    priority_queue<string, std::vector<string>, CompareAlphabets>& suggestedWords) {
     // Try replacing an existing letter in the word with some other letter
     // If any match a word in the hash table, push into the queue for printing later
     for (size_t i = 0; i < userInput.length(); i++) {
         for (char j = 'a'; j <= 'z'; j++) {
             string testingWord = userInput;     // make a copy of userInput to test 
-            if (hashTable.contains(testingWord.replace(i, 1, string(1, j)))) { // FIXME: j doesn't work, has to convert j to string??
-                correctedWords.push(testingWord);
+            if (hashTable.contain(testingWord.replace(i, 1, string(1, j)))) { 
+                suggestedWords.push(testingWord);
             }
         }
     }
@@ -188,76 +184,74 @@ void checkTypo(string userInput, const HashTable& hashTable,
 
 // checking for a transposition
 void checkTransposition(string userInput, const HashTable& hashTable, 
-    priority_queue<string, std::vector<string>, CompareAlphabets>& correctedWords) {
+    priority_queue<string, std::vector<string>, CompareAlphabets>& suggestedWords) {
     // Try transposing adjacent letters
     // If any match a word in the hash table, push into the queue for printing later
     for (size_t i = 0; i < userInput.length() - 1; i++) {
         string testingWord = userInput;         // make a copy of userInput to test
         swap(testingWord[i], testingWord[i + 1]);       // swapping 2 adjacent elements
-        if (hashTable.contains(testingWord)) {
-            correctedWords.push(testingWord);
+        if (hashTable.contain(testingWord)) {
+            suggestedWords.push(testingWord);
         }
     }
 }
 
 // checking for a missing space
 void checkMissingSpace(string userInput, const HashTable& hashTable, 
-    priority_queue<string, std::vector<string>, CompareAlphabets>& correctedWords) {
+    priority_queue<string, std::vector<string>, CompareAlphabets>& suggestedWords) {
     // Try adding a single space at all possible places in the word
     // If any match a word in the hash table, push into the queue for printing later
     for (size_t i = 1; i < userInput.length(); i++) {
         string testingWord = userInput;         // make a copy of userInput to test 
-        if (hashTable.contains(testingWord.insert(i, string(1, ' ')))) { // FIXME: j doesn't work, has to convert j to string??
-            correctedWords.push(testingWord);
+        if (hashTable.contain(testingWord.insert(i, string(1, ' ')))) {
+            suggestedWords.push(testingWord);
         }
     }
 }
 
-// Attempt to correct the user input
-void correction(string userInput, const HashTable& hashTable) {
+// Attempt to suggest the user input
+void suggest(string userInput, const HashTable& hashTable) {
     // Make an empty priority queue
-    priority_queue<string, std::vector<string>, CompareAlphabets> correctedWords;
-    // Note: The functions below CHANGE correctedWords
+    priority_queue<string, std::vector<string>, CompareAlphabets> suggestedWords;
+    // Note: The functions below CHANGE suggestedWords
     // check if the user omitted a letter
-    checkOmission(userInput, hashTable, correctedWords);
+    checkOmission(userInput, hashTable, suggestedWords);
     // check if the user added an extraneous letter
-    checkExtra(userInput, hashTable, correctedWords);
+    checkExtra(userInput, hashTable, suggestedWords);
     // check if the user mistyped a letter
-    checkTypo(userInput, hashTable, correctedWords);
+    checkTypo(userInput, hashTable, suggestedWords);
     // check if the user tranposed 2 adjacent letter
-    checkTransposition(userInput, hashTable, correctedWords);
+    checkTransposition(userInput, hashTable, suggestedWords);
     // check if the user forgot to add a space
-    checkMissingSpace(userInput, hashTable, correctedWords);
+    checkMissingSpace(userInput, hashTable, suggestedWords);
 
-    // print out corrected words in alphabetic order, if there's none, print ???
-    if (correctedWords.empty()) {   // found none
-        cout << "???";
+    // print out suggested words in alphabetic order, if there's none, print ???
+    if (suggestedWords.empty()) {   // found none
+        cout << "??? (Unable to provide a suggestion)";
     } else {
-        while (!correctedWords.empty()) {
-            cout << correctedWords.top() << "\n";
-            correctedWords.pop();
+        cout << "The entered word is non-existent. Suggestions:\n";
+        while (!suggestedWords.empty()) {
+            cout << suggestedWords.top() << "\n";
+            suggestedWords.pop();
         }
     }
 }
 
 int main() {
-    // HashTable* hashTable = new HashTable(1000, "dictionary.txt");
-    HashTable hashTable{1000, "dictionary.txt"};
-    cout << hashTable.size();      // For testing. TODO: erase this
+    HashTable hashTable{10000, "dictionary.txt"};
+    cout << hashTable.size();       // For testing. TODO: erase this
     string userInput;
 
     while (userInput != ".") {
         // Prompt user to enter a word
-        cout << "Please enter a word (type a single period '.' to terminate): ";
+        cout << "\nPlease enter a word (type a single period '.' to terminate): ";
         cin >> userInput;           // save the word to userInput
-        if (hashTable.contains(userInput)) {
-            cout << '*';            // founded the word in hash table, it is acceptable
+        if (hashTable.contain(userInput)) {
+            cout << "* (The word is acceptable)\n";     // founded the word in hash table, it is acceptable
         }
         else {
-            // the word is not found, correcting..
-            correction(userInput, hashTable);
+            // the word is not found, suggesting..
+            suggest(userInput, hashTable);
         }
     }
-    
-    return 0;
 }
