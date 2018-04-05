b33a54c2d199bf5a3297fb6730079d178d561df8
voxqhuy <kohnewlife@gmail.com>
2018-04-05 01:34:16 -0400

    added spm header file (contains main functions to be implemented: making shortest path matrix using dijkstra & using dynamic programming)

---
+++ Hard/Dijkstra-s-algorithm/spm.h
@@ -0,0 +1,15 @@
+#ifndef SPM_H_INCUDED
+#define SPM_H_INCUDED
+
+#include "graph.h"
+
+// Builds and returns the shortest path matrix from a graph's adjacency 
+// matrix using Dijkstra's algorithm.
+AdjacencyMatrix make_spm_dijkstra(const AdjacencyMatrix& in);
+
+// Builds and returns the shortest path matrix from a graph's adjacency 
+// matrix using dynamic programming.
+AdjacencyMatrix make_spm_dp(const AdjacencyMatrix& in);
+
+#endif
+
