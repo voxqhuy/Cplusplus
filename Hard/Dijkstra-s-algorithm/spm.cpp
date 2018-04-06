c03eab07430731f7c4e191372934b2fd9dd3697d
voxqhuy <kohnewlife@gmail.com>
2018-04-06 01:12:34 -0400

    added cases for solved paths or not

--- Hard/Dijkstra-s-algorithm/spm.cpp
+++ Hard/Dijkstra-s-algorithm/spm.cpp
@@ -2,17 +2,24 @@
 #include <iostream>
 #include <iomanip>
 
-int dijkstra_distance()
+int dijkstra_distance(const AdjacencyMatrix& in, int row, int column) {
+    
+}
 
 // Builds and returns the shortest path matrix from a graph's adjacency 
 // matrix using Dijkstra's algorithm.
 AdjacencyMatrix make_spm_dijkstra(const AdjacencyMatrix& in) {
+    // TODO: create an empty matrix maybe?
     for ( unsigned row = 0; row < in.size(); row++ ) {
         for ( unsigned col = 0; col < in[row].size(); col++) {
-            // calculate the shortest path
-            in[i][j] = dijkstra_distance(in, i, j);
-            // undirected graph = symmetric
-            in[j][i] = in[i][j];
+            if (int [i][j] == INF) {
+                // calculate the shortest path
+                in[i][j] = dijkstra_distance(in, i, j);
+                // undirected graph = symmetric
+                in[j][i] = in[i][j];
+            } else {
+                // FIXME: handle already solved paths
+            }
         }
     }
 }
