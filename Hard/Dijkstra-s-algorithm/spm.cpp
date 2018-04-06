20a750d7e265ef7624d45bc716fff192f0ff3e40
voxqhuy <kohnewlife@gmail.com>
2018-04-06 00:37:53 -0400

    added make spm dijkstra base code

--- Hard/Dijkstra-s-algorithm/spm.cpp
+++ Hard/Dijkstra-s-algorithm/spm.cpp
@@ -2,6 +2,21 @@
 #include <iostream>
 #include <iomanip>
 
-int main{
-    
+int dijkstra_distance()
+
+// Builds and returns the shortest path matrix from a graph's adjacency 
+// matrix using Dijkstra's algorithm.
+AdjacencyMatrix make_spm_dijkstra(const AdjacencyMatrix& in) {
+    for ( unsigned row = 0; row < in.size(); row++ ) {
+        for ( unsigned col = 0; col < in[row].size(); col++) {
+            // calculate the shortest path
+            in[i][j] = dijkstra_distance(in, i, j);
+            // undirected graph = symmetric
+            in[j][i] = in[i][j];
+        }
+    }
 }
+
+// Builds and returns the shortest path matrix from a graph's adjacency 
+// matrix using dynamic programming.
+AdjacencyMatrix make_spm_dp(const AdjacencyMatrix& in);
