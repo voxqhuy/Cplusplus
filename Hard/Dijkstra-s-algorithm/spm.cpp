7c59c2981c738f4bd5383556a67ca71d6ccf244b
voxqhuy <kohnewlife@gmail.com>
2018-04-06 23:59:15 -0400

    change enqueue, dequeue to push, pop. Added deallocation. Second set is now an array. Loop and find the shortest path to the destination, using 2 sets to keep track

--- Hard/Dijkstra-s-algorithm/spm.cpp
+++ Hard/Dijkstra-s-algorithm/spm.cpp
@@ -2,12 +2,13 @@
 #include <climits>      // INT_MAX
 #include <queue>        // std::priority_queue
 #include <map>          // std::map
+#include <algorithm>    // std::fill_n
 
 // Ordering distances so that smallest gets served first
 class distanceSort {
 public:
     bool operator()(pair<int, int> num1, pair<int, int> num2) {
-        return num1.second > num2.second;       // (pair) vertex : distance
+        return num1.second > num2.second;   // (pair) vertex : distance
     }
 };
 
@@ -15,19 +16,55 @@ int dijkstra_distance(const AdjacencyMatrix& in, int start, int dest) {
     // the size of one side of passed in matrix = the number of vertices of the graph
     int size = in.size();
     // A queue that keep all the vertices with their distances from the start vertex
-    priority_queue<pair<int, int>, std::vector<pair<int, int>>, distanceSort> uncalculatedVertices;
-    // A map does the job of shortest path table. Intially, it is empty.
-    std::map<int, int> calculatedVertices;
+    priority_queue<pair<int, int>, std::vector<pair<int, int>>, distanceSort> uncheckeds;
+    // An array does the job of shortest path table. Intially, it is empty.
+    int checkeds[size]; 
+    // fill the array with INFs, except for the start vertex = 0
+    std::fill_n(checkeds, size, INF);
+    checkeds[start] = 0;
     // Initilize the queue
     for ( unsigned i = 0; i < size; i++) {
         // the start vertex has a distance of 0
         if ( i == start ) {
-            uncalculatedVertices.enqueue(i, 0);
+            uncheckeds.push(i, 0);
         } else {
             // other vertices have infinity distance
-            uncalculatedVertices.enqueue(i, INF);
+            uncheckeds.push(i, INF);
         }
     }
+
+    // the result
+    int dijkstra_distance = 0;
+    while (!uncheckeds.empty()) {
+        // Get the vertex with the minimum distance
+        std::pair<int, int> v = uncheckeds.top();
+        int dist = v.second();              // and its distance
+        if (v.first() == dest) {            // found the shortest path to the destination
+            dijkstra_distance = dist;
+            break;              
+        }
+        uncheckeds.pop();                   // remove the vertex. It is solved
+
+        // A vector that keeps the weights from the closestVertex to all others
+        std::vector<int> weights = in[v];
+        // Adding all adjacent vertices with the new distances to the queue
+        for ( unsigned i = 0; i < size; i++) {
+            int w = weights[i];             // the weight in between
+            if (w != -1) {                  // There is an edge in between
+                int d = dist + w;           // new distance = current distance + the weight
+                if (d < checkeds[i]) {      // found a shorter path
+                    checkeds[i] = d;        // Update the table
+                    uncheckeds.push(i, d);
+                }
+            }
+        }
+    }
+    // Deallocation
+    uncheckeds = {};
+    delete [] checkeds;  // Free memory allocated for the array.
+    checkeds = NULL;     // Be sure the deallocated memory isn't used.
+
+    return dijkstra_distance;
 }
 
 // Builds and returns the shortest path matrix from a graph's adjacency 
@@ -44,9 +81,9 @@ AdjacencyMatrix make_spm_dijkstra(const AdjacencyMatrix& in) {
             if (i == j) {
                 spm[i][j] = 0;
             } 
-            else if (in [i][j] != INF) {            // the path is solved
-                spm[i][j] == in[i][j];              // if it is copiable, otherwise just + 0
-            } else {                                // the path is unsolved
+            else if (in [i][j] != INF) {    // the path is solved
+                spm[i][j] == in[i][j];      // if it is copiable, otherwise just + 0
+            } else {                        // the path is unsolved
                 // calculate the shortest path
                 in[i][j] = dijkstra_distance(in, i, j);
                 // undirected graph = symmetric
