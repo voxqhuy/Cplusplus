b11d78734c0015f1d4a6bcab8454bce66b03c0d1
voxqhuy <kohnewlife@gmail.com>
2018-04-29 01:51:12 -0400

    added equals

--- Easy/Node/node.cpp
+++ Easy/Node/node.cpp
@@ -15,6 +15,45 @@ struct Node {
         data(d), left(lf), right(rt) {}
 };
 
+// Calculate the distance from the root to the closest node
+// that holds the given data value
+int distance(const Node *t, int value) {
+    if (t == nullptr) {
+        return -1;
+    }
+    int d = -1;
+    if ((t->data == value) 
+        || (d = distance(t->left, value)) >= 0 
+        || (d = distance(t->right, value)) >= 0) {
+        return d + 1;
+    }
+    return d;
+}
+
+int count(const Node*t, int value) {
+    if (t == nullptr) {
+        return 0;
+    }
+    int c = 0;
+    if (t->data == value) {c++;}
+    c += count(t->left, value);
+    c += count(t->right, value);
+    return c;
+}
+
+bool equals(const Node*t1, const Node *t2) {
+    if (!t1  && !t2) {
+        return true;
+    } else if (!t1 || !t2) {
+        return false;
+    }
+    if (t1->data != t2->data) {
+        return false;
+    }
+    if (!equals(t1->left, t2->left) || !equals(t1->right, t2->right)) {return false;}
+    return true;        // no falses
+}
+
 int main() {
     Node *node1 = new Node(
         1,
@@ -309,4 +348,23 @@ int main() {
             )
         )
     );
+
+    // cout << distance(node2, 4);
+    // cout << distance(node2, 5);
+    // cout << distance(node2, 2);
+    // cout << distance(node2, 1);
+    // cout << distance(node2, 3);
+
+    // cout << count(node2, 4);
+    // cout << count(node2, 5);
+    // cout << count(node2, 2);
+    // cout << count(node2, 1);
+    // cout << count(node2, 3);
+
+    cout << equals(node1, node2);
+    cout << equals(node1, node1);
+    cout << equals(node1, node17);
+    cout << equals(node14, node15);
+    cout << equals(node12, node13);
+    cout << equals(node15, node16);
 }
